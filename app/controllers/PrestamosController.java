67fa4bf4a35a0939702366e8ef90a50d4b3f5a1c
Juan Carlos <jcgamar@unal.edu.co>
2018-04-10 14:38:35 -0500

    mejoras y logers

--- app/controllers/PrestamosController.java
+++ app/controllers/PrestamosController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import play.mvc.*;
 import play.libs.Json;
+import play.Logger;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
@@ -17,11 +18,13 @@ import java.math.BigInteger;
 public class PrestamosController extends Controller {
 
     public Result index() {
+        Logger.info("getAll");
         List<Prestamo> prestamos = Prestamo.find.all();
         return ok( Json.toJson(prestamos) );
     }
 
     public Result create() {
+        Logger.info("Create");;
         Prestamo prestamo = new Prestamo();
 
         getRequests(prestamo);
@@ -35,6 +38,7 @@ public class PrestamosController extends Controller {
     }
     
     public Result show(Integer id) {
+        Logger.info("Show" + id);
         Prestamo prestamo = Prestamo.find.byId(id);
         
         if(prestamo == null)
@@ -44,6 +48,7 @@ public class PrestamosController extends Controller {
     }
     
     public Result update(Integer id) {
+        Logger.info("Update" + id);
         Prestamo prestamo = Prestamo.find.byId(id);
         
         if(prestamo == null)
@@ -54,17 +59,19 @@ public class PrestamosController extends Controller {
         if(!prestamoValido(prestamo))
             return status(406, Json.toJson(ErrorResponse.NOT_ACCEPTABLE) );
 
-        return ok();
+        return ok(Json.toJson(prestamo));
     }
 
     public Result destroy(Integer id) {
+        Logger.info("Destroy" + id);
+        Prestamo prestamo = Prestamo.find.byId(id);
         Prestamo.find.deleteById(id);
-        return ok();
+        return ok(Json.toJson(prestamo));
     }
 
     private Prestamo getRequests(Prestamo prestamo) {
         JsonNode json = request().body().asJson();
-        
+
         if(json.has("student_id"))
             prestamo.setStudentId( json.findValue("student_id").bigIntegerValue() );
         if(json.has("bici_id"))
